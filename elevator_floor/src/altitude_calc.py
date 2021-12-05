a7ea60ed72a6f52eb96f6dd9eea499657447292c
michinchin <chinabigail08@yahoo.com>
2021-12-04 23:49:16 -0800

    code update without testing on bagfile

--- elevator_floor/src/altitude_calc.py
+++ elevator_floor/src/altitude_calc.py
@@ -13,8 +13,32 @@ from unitree_legged_msgs.msg import HighState
 from sensor_msgs.msg import Imu
 
 
+def get_state(high_state):
+    ''' Returns time, acceleration at current state
+    '''
+    _, accelerometer, _ = high_state.imu
+    time = high_state.tick
+    print(accelerometer)
+    return [time,accelerometer]
 
-def calculate_floor(accelerometer_arr):
+# acceleration data
+
+def calculate_velocity(high_state, time_passed):
+    ''' Calculate if current velocity is 0 
+    '''
+    # get acclerometer data
+    _, _, z = high_state.imu[1]
     
-    return 0.0
+    # z-acceleration initally accounting for gravity
+    velocity = (z - 9.8) * time_passed
+    
+    return velocity
 
+# around 4/5 m: distance between floors
+
+def calculate_distance(high_state, time_passed):
+    ''' Calculate relative distance
+    '''
+    return calculate_velocity(high_state) * time_passed
+    
+    
--- elevator_floor/src/elevator_floor_main.py
+++ elevator_floor/src/elevator_floor_main.py
@@ -13,73 +13,90 @@ from collections import deque
 import rospy
 
 from unitree_legged_msgs.msg import HighState
+from elevator_door.msg import ElevatorDoorState
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Bool
 
 import altitude_calc as ac
 
+
 class CheckElevatorFloorProcess:
     '''
     Wraps processing of imu data from input ros topic and publishing to elevator_floor topic
     '''
-    def __init__(self, a1_states, floor_node):
+
+    def __init__(self, a1_states, floor_node, elevator_door):
         self.num_steps = 0
         self.start_time = 0.0
+        self.end_time = 0.0
         self.is_moving_in_z_dir = False
+        self.accelerations = []
+        self.door_states = []
         
         self.messages = deque([], 5)
-        a1_sub = rospy.Subscriber(a1_states, HighState, self.callback)
-        
-        self.elevator_floor_pub = rospy.Publisher(floor_node, Bool, queue_size=10)
-        # self.callback(a1_sub)
-    
-    def callback(self, high_state):
+        a1_sub = rospy.Subscriber(a1_states, HighState)
+        door_sub = rospy.Subscriber(elevator_door, ElevatorDoorState)
+
+        self.elevator_floor_pub = rospy.Publisher(
+            floor_node, Bool, queue_size=10)
+        self.callback(a1_sub, door_sub)
+
+    def callback(self, high_state, door_state):
         try:
-            a1_state = self.get_altitude(high_state)
+            # compare past door state
+            past_door_state = ElevatorDoorState.CLOSED
+            if len(door_state > 1):
+                past_door_state = self.door_states.pop(0)
+            
+            # if not moving forward, should be in elevator
+            # is_not_moving =  high_state.velocity[0] == 0 
+            is_not_moving = ac.calculate_velocity(high_state) <= 1.0
+            if is_not_moving:
+                self.start_time = # set start time here? or end time?
+                if door_state is ElevatorDoorState.OPENING and past_door_state is ElevatorDoorState.CLOSED:
+                    print('opening')
+                # check if starting to move
+                elif door_state is ElevatorDoorState.CLOSING:  
+                    print('closing')
+                    self.start_time = # get time here
+                    # want the time between not moving states to calculate distance
+                    a1_state = self.get_altitude(high_state)
+            self.door_states.append(door_state)
+        
         except Exception as e:
             rospy.logerr(e)
             return
-        self.messages.appendleft(a1_state)
-    
-    def get_altitude(self, high_state):
-        x,y,z_acceleration = high_state.imu.accelerometer
-        print('position:' + str(high_state.position))
-        print('z_acceleration:'+ str(z_acceleration))
-        # initially around 9.8 m/s^2
-        # calculate when moving and then 
-            # calculate previous iteration of moving 
-        # else:
-        #    self.is_moving_in_z_dir = True
-        return 0.0
         
+        self.messages.appendleft(a1_state)
+
     def publish_once_from_queue(self):
         if self.messages:
-            a1_state = self.messages.pop()
+            # let's just publish after after not_moving->not_moving time period
+            time, acceleration = self.messages.pop()
             try:
-                floor = ac.calculate_floor(a1_state)
+                floor = ac.calculate_distance(acceleration)
             except Exception as e:
                 return
-             
+
             self.elevator_floor_pub.publish(False)
             # print("Published floor msg at timestamp:",
             #       floor.header.stamp.secs)
+
 
 def main():
     IMU_NODE = '/a1_states'
     IMU_MSG_TYPE = 'unitree_legged_msgs/HighState'
     ELEVATOR_FLOOR_NODE = 'elevator_floor'
 
-
     rospy.init_node('elevator_floor', anonymous=True)
-    process = CheckElevatorFloorProcess(IMU_NODE,ELEVATOR_FLOOR_NODE)
-    
+    process = CheckElevatorFloorProcess(IMU_NODE, ELEVATOR_FLOOR_NODE)
+
     r = rospy.Rate(1000)
 
     while not rospy.is_shutdown():
         process.publish_once_from_queue()
         r.sleep()
 
+
 if __name__ == '__main__':
     main()
-    
-    
