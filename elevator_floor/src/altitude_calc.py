2d42e43abdaca1782cac4d6f2b9bb7fa19032e96
Christy Koh <christykoh@berkeley.edu>
2021-12-06 13:36:26 -0800

    testing files and changes to calculating floor

--- elevator_floor/src/altitude_calc.py
+++ elevator_floor/src/altitude_calc.py
@@ -6,20 +6,19 @@ This is a helper function file that takes in imu data from a
 
 Functions to extract elevator acceleration and height
 """
-from numpy import floor
+# from numpy import floor
 import rospy
 
 from unitree_legged_msgs.msg import HighState
 from sensor_msgs.msg import Imu
 
 
-def get_state(high_state):
+def get_acceleration(high_state):
     ''' Returns time, acceleration at current state
     '''
-    _, accelerometer, _ = high_state.imu
-    time = high_state.tick
-    print(accelerometer)
-    return [time,accelerometer]
+    accelerometer = high_state.imu.accelerometer
+
+    return 9.8 - accelerometer[2]
 
 # acceleration data
 
@@ -27,7 +26,8 @@ def calculate_velocity(high_state, time_passed):
     ''' Calculate if current velocity is 0 
     '''
     # get acclerometer data
-    _, _, z = high_state.imu[1]
+    _, _, z = high_state.imu.accelerometer
+    # print('Acceleration_z: ' + str(z))
     
     # z-acceleration initally accounting for gravity
     velocity = (z - 9.8) * time_passed
@@ -39,6 +39,8 @@ def calculate_velocity(high_state, time_passed):
 def calculate_distance(high_state, time_passed):
     ''' Calculate relative distance
     '''
+    # how to calculate time passed with unix timestamps?
+    # print('Time Passed: ' + str(time_passed))
     return calculate_velocity(high_state) * time_passed
     
     
--- elevator_floor/src/elevator_floor_main.py
+++ elevator_floor/src/elevator_floor_main.py
@@ -7,15 +7,17 @@ and publishes to elevator_floor to notify which floor the guide dog is on.
 
 Functions to extract elevator acceleration and height
 """
-import numpy as np
 from collections import deque
 
+import message_filters
 import rospy
+import datetime
 
 from unitree_legged_msgs.msg import HighState
 from elevator_door.msg import ElevatorDoorState
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Bool
+from diagnostic_msgs.msg import DiagnosticArray
 
 import altitude_calc as ac
 
@@ -25,56 +27,85 @@ class CheckElevatorFloorProcess:
     Wraps processing of imu data from input ros topic and publishing to elevator_floor topic
     '''
 
-    def __init__(self, a1_states, floor_node, elevator_door):
+    def __init__(self, a1_states, floor_node, elevator_door, diagnostic_node):
         self.num_steps = 0
-        self.start_time = 0.0
-        self.end_time = 0.0
-        self.is_moving_in_z_dir = False
+        self.time_arr = []
+        self.is_moving = False
+        self.forward_velocity = 0.0
         self.accelerations = []
         self.door_states = []
         
         self.messages = deque([], 5)
-        a1_sub = rospy.Subscriber(a1_states, HighState)
-        door_sub = rospy.Subscriber(elevator_door, ElevatorDoorState)
+        a1_sub = rospy.Subscriber(a1_states, HighState, self.a1_state_callback)
+        diagnostic_sub = rospy.Subscriber(diagnostic_node, DiagnosticArray, self.time_callback)
+        door_sub = rospy.Subscriber(elevator_door, ElevatorDoorState, self.elevator_state_callback)
 
         self.elevator_floor_pub = rospy.Publisher(
             floor_node, Bool, queue_size=10)
-        self.callback(a1_sub, door_sub)
 
-    def callback(self, high_state, door_state):
+    def time_callback(self, diagnostic):
         try:
+            time = diagnostic.header.stamp
+            self.time_arr.append(time)
+            # print('Time:'+ str(diagnostic.header.stamp))
+        except Exception as e:
+            rospy.logerr(e)
+            return
+
+    def a1_state_callback(self, high_state):
+        try:
+            acceleration = ac.get_acceleration(high_state)
+            # print('Acceleration: '+ str(acceleration))
+            self.is_moving =  acceleration > 1 
+            print(self.is_moving)
+        except Exception as e:
+            rospy.logerr(e)
+            return
+        
+        self.messages.appendleft(high_state)
+
+    def elevator_state_callback(self, door_state):
+        try:
+            # why is door state not showing? 
             # compare past door state
+            if door_state is ElevatorDoorState.CLOSED:
+                print('closed')
+            elif door_state is ElevatorDoorState.OPEN:
+                print('open')
+            # print('Door State:' + str(door_state))
             past_door_state = ElevatorDoorState.CLOSED
             if len(door_state > 1):
                 past_door_state = self.door_states.pop(0)
             
             # if not moving forward, should be in elevator
             # is_not_moving =  high_state.velocity[0] == 0 
-            is_not_moving = ac.calculate_velocity(high_state) <= 1.0
-            if is_not_moving:
-                self.start_time = # set start time here? or end time?
+            if self.is_not_moving:
+                # self.start_time = # set start time here? or end time?
                 if door_state is ElevatorDoorState.OPENING and past_door_state is ElevatorDoorState.CLOSED:
                     print('opening')
                 # check if starting to move
                 elif door_state is ElevatorDoorState.CLOSING:  
                     print('closing')
-                    self.start_time = # get time here
+                    # self.start_time = # get time here
                     # want the time between not moving states to calculate distance
-                    a1_state = self.get_altitude(high_state)
+                    # a1_state = self.get_altitude(high_state)
             self.door_states.append(door_state)
         
         except Exception as e:
             rospy.logerr(e)
             return
         
-        self.messages.appendleft(a1_state)
+        # self.messages.appendleft([high_state, diag_sub, door_state])
 
     def publish_once_from_queue(self):
         if self.messages:
             # let's just publish after after not_moving->not_moving time period
-            time, acceleration = self.messages.pop()
+            high_state = self.messages.pop()
+            # acceleration = high_state.imu.accelerometer[2]
             try:
-                floor = ac.calculate_distance(acceleration)
+                time_passed = self.time_arr[-1] - self.time_arr[0]
+                distance = ac.calculate_distance(high_state, time_passed)
+                # print('Distance: ' + str(distance))
             except Exception as e:
                 return
 
@@ -87,9 +118,11 @@ def main():
     IMU_NODE = '/a1_states'
     IMU_MSG_TYPE = 'unitree_legged_msgs/HighState'
     ELEVATOR_FLOOR_NODE = 'elevator_floor'
+    ELEVATOR_DOOR_NODE = 'elevator_door'
+    DIAGNOSTIC_NODE = '/diagnostics'
 
     rospy.init_node('elevator_floor', anonymous=True)
-    process = CheckElevatorFloorProcess(IMU_NODE, ELEVATOR_FLOOR_NODE)
+    process = CheckElevatorFloorProcess(IMU_NODE, ELEVATOR_FLOOR_NODE, ELEVATOR_DOOR_NODE, DIAGNOSTIC_NODE)
 
     r = rospy.Rate(1000)
 
