bea4996185a1cc9ca667e66d2fbd7cc5300f6b2a
Shuxiao Chen <shuxiao.chen@outlook.com>
2021-12-17 04:40:26 -0800

    working up to TURNING_TO_DOOR state, implemented P controller

--- elevator_door/src/elevator_control.py
+++ elevator_door/src/elevator_control.py
@@ -11,14 +11,15 @@ from a1_control_states import *
 from elevator_door.msg import ElevatorDoorState
 from std_msgs.msg import UInt8, Float64
 from unitree_legged_msgs.msg import HighCmd, HighState
-from std_srvs.srv import Empty
+from std_srvs.srv import Empty, EmptyResponse
 
-DEPTH_THRESHOLD = 600  # in pointcloud units
+DEPTH_THRESHOLD = 0.8  # in pointcloud units
 UPDATE_DELAY = 0.002  # Hz
+ROTATE_GOAL = 3.14  # rad
 
 class ElevatorControl:
     
-    def __init__(self, floor, ctrl_state):
+    def __init__(self, current_floor, target_floor, ctrl_state):
         self.floor_sub = rospy.Subscriber('/elevator/floor', UInt8, self.set_current_floor)
         self.door_sub = rospy.Subscriber('/elevator/door_state', ElevatorDoorState, self.update_door_state)
         self.a1_state_sub = rospy.Subscriber('/a1_states', HighState, self.update_a1_state)
@@ -27,11 +28,14 @@ class ElevatorControl:
         self.state_pub = rospy.Publisher('/elevator/ctrl/state', UInt8, queue_size=1)
         self.cmd_pub = rospy.Publisher('/elevator/ctrl/highcmd', HighCmd, queue_size=10)
 
+        # init service
+        rospy.Service('/elevator/ctrl/reset', Empty, self.reset_service_callback)
+        # rospy.Service('/elevator/set_target', UInt8, self.reset_service_callback)
         self.calibrate_depth = rospy.ServiceProxy('/calibrate_depth', Empty)
 
         # State variables
-        self.current_floor = floor
-        self.target_floor = 1
+        self.current_floor = current_floor
+        self.target_floor = target_floor
         self.view_depth = 0.0  # avg depth of Realsense PointCloud
         self.door_state = ElevatorDoorState()
 
@@ -41,6 +45,11 @@ class ElevatorControl:
         print_state(self.control_state)
 
         self.is_complete = False
+        self.print_depth = True
+
+        # control variables
+        self.fwd_kP = 0.1
+        self.rot_kP = 0.8
 
     # -------- INSTANCE SETTER METHODS --------
 
@@ -52,14 +61,22 @@ class ElevatorControl:
         # print("gyro: \t%.3f \t%.3f \t%.3f" % self.high_state.imu.gyroscope)
 
     def update_depth(self, depth):
-        self.view_depth = depth
-        print("depth: %.3f" % self.view_depth)
+        self.view_depth = depth.data
+        if self.print_depth:
+            print("depth: %.3f" % self.view_depth)
 
     def set_target_floor(self, floor):
         self.target_floor = floor
 
     def set_current_floor(self, floor):
         self.current_floor = floor
+
+    def reset_service_callback(self, msg):
+        self.command_stop()
+        self.yaw = 0.0
+        self.control_state = ControlState.OUTSIDE_WAITING
+        self.is_complete = False
+        return EmptyResponse()
 
     # ------- PUBLISHING METHODS --------
 
@@ -75,6 +92,7 @@ class ElevatorControl:
         cmd = HighCmd()
         cmd.mode = 2
         self.cmd_pub.publish(cmd)
+        time.sleep(0.5)
 
     def command_rotate(self, rot_speed):
         cmd = HighCmd()
@@ -105,13 +123,20 @@ class ElevatorControl:
             if self.door_state.state == ElevatorDoorState.OPEN:
                 print("DOOR OPEN")
                 # transition: start entering
-                self.command_fwd(0.2)
+                self.command_walk()
                 self.control_state = self.control_state.next()
         elif self.control_state == ControlState.ENTERING:
             # walk until pointcloud estimate
-            if self.depth_sub < DEPTH_THRESHOLD:
+            if self.view_depth > DEPTH_THRESHOLD:
+                # TODO implement P controller
+                self.command_fwd(self.view_depth * self.fwd_kP)
+            else:
                 # transition: stop moving
                 self.command_stop()
+                self.print_depth = False
+                print("Please press the elevator button.")
+                time.sleep(2)
+                print("Thank you :)")
                 # TODO initiate button press
                 self.control_state = self.control_state.next()
         elif self.control_state == ControlState.PRESSING_BUTTON:
@@ -122,11 +147,15 @@ class ElevatorControl:
             self.control_state = self.control_state.next()
         elif self.control_state == ControlState.TURNING_TO_DOOR:
             print("yaw: %.3f" % self.yaw)
-            if self.yaw > 3.14:
+            error = ROTATE_GOAL - self.yaw
+            if error > 0.1:
+                self.command_rotate(error * self.rot_kP)
+            else:
                 print("TURNED 180 DEGREES.")
                 # TODO Check if robot has rotated 180 degrees
                 # transition: enter joint locked mode and calibrate depth
                 self.command_stop()
+                self.print_depth = True
                 time.sleep(1)
                 self.calibrate_depth()
                 time.sleep(1)
@@ -142,6 +171,7 @@ class ElevatorControl:
                 print("DOOR OPEN")
                 # transition: stop movement
                 self.command_stop()
+                self.print_depth = False
                 self.control_state = self.control_state.next()
         elif self.control_state == ControlState.COMPLETE:
             self.is_complete = True
@@ -151,21 +181,17 @@ class ElevatorControl:
 
 if __name__ == '__main__':
     rospy.init_node('elevator_control')
+    current_floor = rospy.get_param('~curr_floor', 2)
+    target_floor = rospy.get_param('~taret_floor', 1)
     init_state = rospy.get_param('~init_state', 1)
     rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~init_state'), init_state)
 
-    control = ElevatorControl(2, ControlState(init_state))
-
-    control.command_walk()
-    time.sleep(0.5)
-    control.command_fwd(0.3)
-    time.sleep(2)
-    control.command_stop()
-
-    print("complete")
+    control = ElevatorControl(2, 1, ControlState(init_state))
 
     r = rospy.Rate(10)
 
     while not rospy.is_shutdown() and not control.is_complete:
         control.state_controller()
         r.sleep()
+
+    control.command_stop()
